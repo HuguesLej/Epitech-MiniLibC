50a35311fdcb573f93bd3cde160f77e848f21840
Hugues Lejeune <hugues.lejeune@epitech.eu>
2024-02-08 16:05:47 +0100

    [FIX] make memmove working

--- src/memmove.asm
+++ src/memmove.asm
@@ -4,8 +4,11 @@ SECTION .text
 GLOBAL memmove
 
 memmove:
+    PUSH RBP
+    MOV RBP, RSP
     CMP RDX, 0
     JE end
+    SUB RSP, RDX
     XOR RCX, RCX
     JMP copy_src
 
@@ -13,23 +16,23 @@ copy_src:
     CMP RCX, RDX
     JE reinit_rcx
     MOV AL, BYTE [RSI + RCX]
-    MOV BYTE [R8 + RCX], AL
+    MOV BYTE [RSP + RCX], AL
     INC RCX
     JMP copy_src
 
 reinit_rcx:
     XOR RCX, RCX
-    MOV RSI, R8
     JMP move_loop
 
 move_loop:
     CMP RCX, RDX
     JE end
-    MOV AL, BYTE [R8 + RCX]
+    MOV AL, BYTE [RSP + RCX]
     MOV BYTE [RDI + RCX], AL
     INC RCX
     JMP move_loop
 
 end:
     MOV RAX, RDI
+    LEAVE
     RET
