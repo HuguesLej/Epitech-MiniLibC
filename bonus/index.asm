c55c9d67bf709f280239cf2f3bd7312094f2fc00
HuguesLej <hugues.lejeune@epitech.eu>
2024-02-25 23:15:24 +0100

    [FIX] correct index func

--- bonus/index.asm
+++ bonus/index.asm
@@ -6,9 +6,15 @@ GLOBAL index
 index:
     CMP BYTE [RDI], SIL
     JE end
+    CMP BYTE [RDI], 0
+    JE end_zero
     INC RDI
     JMP index
 
 end:
     MOV RAX, RDI
+    RET
+
+end_zero:
+    XOR RAX, RAX
     RET
